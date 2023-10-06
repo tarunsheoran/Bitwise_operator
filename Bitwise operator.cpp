d9187ac3bc59995c9c76c8166af3ab272050241f
tarunsheoran <118544285+tarunsheoran@users.noreply.github.com>
2023-10-06 21:17:13 +0530

    Add files via upload

---
+++ Bitwise operator.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int a=5;
+	int b=8;
+	cout<<"a&b = "<<(a&b)<<endl;
+	cout<<"a|b = "<<(a|b)<<endl;
+	cout<<"~a = "   <<~a<<endl;
+	cout<<"a^b = "<<(a^b)<<endl;
+
+} 
